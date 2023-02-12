6df5f501380189e751cc02040c7dc5870a82d255
Murad Hajiyev <hajiyev.murad1@gmail.com>
2023-02-12 09:51:04 -0500

    Add logic to return respomnse

--- src/main/java/com/example/telegrambotdemo/Bot.java
+++ src/main/java/com/example/telegrambotdemo/Bot.java
@@ -3,6 +3,7 @@ package com.example.telegrambotdemo;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class Bot extends TelegramWebhookBot {
@@ -18,7 +19,10 @@ public class Bot extends TelegramWebhookBot {
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
+        String chatId = update.getMessage().getChatId().toString();
+        String clientId = update.getMessage().getFrom().getId().toString();
+
+        return new SendMessage(chatId, "Aleykume salam");
     }
 
     @Override
